5222bb10d72e68a6dcffa1ec8f18a2b547d687e2
cathesposito <73259833+cathesposito@users.noreply.github.com>
2021-05-12 19:51:11 -0300

    version 1

---
+++ index.js
@@ -0,0 +1,30 @@
+
+$("button").click(function(){
+  $("#text").toggle("slow");
+  if ($(".hide").css("visibility") == "visible")
+    $(".hide").css("visibility", "hidden");
+  else
+    $(".hide").css("visibility", "visible");
+});
+
+
+function position(i) {
+
+  var image = ".image" + i;
+  var hide = ".hide" + i;
+
+  $(image)
+  .on("mouseenter", function() {
+      $(this).css("width", "100%");
+      $(hide).toggle();
+    })
+  .on("mouseleave", function() {
+      $(this).css("width", "90%");
+      $(hide).toggle();
+    })
+
+};
+
+for (i = 1; i <= 12; i++) {
+  position(i)
+};
